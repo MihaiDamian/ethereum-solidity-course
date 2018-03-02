52825edeafb68bdd18e1ad08e1aca7cd414e8448
Mihai Damian <Mihai.Damian@iquestgroup.com>
2018-03-02 17:00:45 +0200

    Finished up Kickstart example

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Button, Card, Grid } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
+import { Link } from '../../routes';
 
 class CampaignShow extends Component {
 
@@ -9,6 +12,7 @@ class CampaignShow extends Component {
     const campaign = Campaign(props.query.address);
     const summary = await campaign.methods.getSummary().call();
     return {
+      address: props.query.address,
       minimumContribution: summary[0],
       balance: summary[1],
       requestsCount: summary[2],
@@ -27,6 +31,22 @@ class CampaignShow extends Component {
         meta: 'Address of Manager',
         description: 'The manager created this campaign and can create requests to withdraw money',
         style: { overflowWrap: 'break-word' },
+      }, {
+        header: minimumContribution,
+        meta: 'Minimum Contribution (wei)',
+        description: 'You must contribute at least this much wei to become an approver',
+      }, {
+        header: requestsCount,
+        meta: 'Number of Requests',
+        description: 'A request tries to withdraw money from the contract. Requests must be approved by approvers.',
+      }, {
+        header: approversCount,
+        meta: 'Number of Approvers',
+        description: 'Number of people who have donated to this campaign',
+      }, {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign Balance (ether)',
+        description: 'The balance is how much money this campaign has left to spend',
       },
     ];
     return <Card.Group items={items}/>;
@@ -36,7 +56,25 @@ class CampaignShow extends Component {
     return (
       <Layout>
         <h3>Campaign Show</h3>
-        {this.renderCards()}
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={10}>
+              {this.renderCards()}
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <ContributeForm address={this.props.address}/>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Link route={`/campaigns/${this.props.address}/requests`}>
+                <a>
+                  <Button primary>View Requests</Button>
+                </a>
+              </Link>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Layout>
     );
   }
